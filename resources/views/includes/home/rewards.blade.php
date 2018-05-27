ab5994952bc6705306ccb8d6ef9e41077e1d1642
basicgts <basicgts@gmail.com>
2018-05-27 22:36:39 +0700

    merge mat edit index

--- resources/views/includes/home/rewards.blade.php
+++ resources/views/includes/home/rewards.blade.php
@@ -7,9 +7,24 @@
                         <i class="ion-android-hangout"></i>
                     </div>
                     <div class="content">
-                        <h4 class="bottom-line"><span class="markFont">Like 'nd Share เราจัดให้ </span></h4>
-                        <p>&nbsp;&nbsp;&nbsp;&nbsp;เพียงคุณกด Like Official Page กดแชร์ Post นี้แบบสาธารณะ เพื่อชวนเพื่อนๆที่กำลังเรียนอยู่
-                            มาร่วมก๊วนตั้งแก๊ง  เพื่อเฟ้นหาสุดยอดทีม ที่สามารถผ่านเข้ารอบสุดท้าย ที่จะทำให้หัวใจ <i class="ion-ios-pulse-strong"></i> เต้นรัวๆ กับเหล่าบันดาสาวสวย Cosplay และ ครูอาย Net Idol ที่ใครๆก็อยากมาเป็นลูกศิษย์ <i class="ion-ios-color-wand"></i></p>
+                        <h4 class="bottom-line"><span class="markFont">Like And Share เราจัดให้ </span></h4>
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp;เพียงคุณกด Like Official Page กดแชร์ Post กิจกรรมเป็นแบบสาธารณะ เพื่อชวนเพื่อนๆที่กำลังเรียนอยู่
+                            มาร่วมก๊วนตั้งแก๊ง  เพื่อเฟ้นหาสุดยอดทีม ที่สามารถผ่านเข้ารอบ 4 ทีมสุดท้าย ที่จะทำให้หัวใจ <i class="ion-ios-pulse-strong"></i> เต้นรัวๆ กับเหล่าบรรดา Cosplay สาว แสนสวย และ Caster สาวน่ารัก <i class="ion-ios-color-wand"></i></p>
+                    </div>
+                </div>
+                <div class="feature-wiget">
+                    <div class="icon-wraper">
+                        <i class="ion-android-share-alt"></i>
+                    </div>
+                    <div class="content">
+                        <h4 class="bottom-line"><span class="markFont">ร่วมสนุกกับกิจกรรมแจกของรางวัล</span></h4>
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp;เพียงคุณกด Like Official Page และกดแชร์ Post กิจกรรมเป็นแบบสาธารณะ ก็มีสิทธิ์ลุ้นรับของรางวัลจากเรามากมาย ยกตัวอย่างเช่น กิจกรรมแจกของรางวัลครั้งที่ 1 ที่ได้รับการตอบรับอย่างมาก ของรางวัลมีดังนี้</p>
+                        <ul>
+                            <li><p>รางวัลที่ 1 เม้าส์เกมแบบความเร็วสูง ยี่ห้อ logitech G402</p></li>
+                            <li><p>รางวัลที่ 2 รูป PunBNK48 จาก KFC Senbatsu photoset</p></li>
+                            <li><p>รางวัลที่ 3 รูป MobileBNK48 จาก Road show photoset</p></li>
+                        </ul>
+                        <p>สำหรับใครที่พลาดโอกาสในการร่วมสนุกกับกิจกรรมแจกของรางวัลครั้งที่ 1 ของเราไปแล้ว ไม่ต้องเสียใจไป เพราะเรายังมีกิจกรรมดีๆ แบบนี้อีกเพียบ อย่ารอช้าเข้าไปกด Like + Follow Official Page ของเรากันเล้ยย</p>
                     </div>
                 </div>
                 <div class="feature-wiget">
@@ -17,8 +32,22 @@
                         <i class="ion-trophy"></i>
                     </div>
                     <div class="content">
-                        <h4 class="bottom-line"><span class="markFont">รางวัลสำหรับทีมที่เข้ามาถึงรอบสุดท้าย</span></h4>
-                        <p>&nbsp;&nbsp;&nbsp;&nbsp;เงินรางวัลสำหรับทีมผู้ชนะ อันดับ 1-3  และของรางวัลติดไม้ติดมือกลับบ้าน จากทาง Garana Thailand.</p>
+                        <h4 class="bottom-line"><span class="markFont">รางวัลสำหรับทีมที่เข้ามาถึงรอบ 4 ทีมสุดท้าย</span></h4>
+                        <ul>
+                            <li><p>อันดับ 1 ได้รับเงินรางวัลจำนวน 5,000 บาท, คูปองในเกม 500 คูปองต่อคน, สกินฮีโร่คนละ 1 สกินต่อคน</p></li>
+                            <li><p>อันดับ 2 ได้รับเงินรางวัลจำนวน 3,000 บาท, คูปองในเกม 300 คูปองต่อคน, สกินฮีโร่คนละ 1 สกินต่อคน</p></li>
+                            <li><p>อันดับ 3 - 4 ได้รับเงินรางวัลจำนวน 1,000 บาท, คูปองในเกม 100 คูปองต่อคน</p></li>
+                        </ul>
+                        <p>หมายเหตุ ภายใน 1 ทีม จะสามารถรับรางวัลได้แค่ 5 ไอดีเท่านั้น คือผู้เล่นตัวจริงไม่รวมผู้เล่นสำรอง คูปองและสกินสนับสนุนโดย Garena Thailand</p>
+                    </div>
+                </div>
+                <div class="feature-wiget">
+                    <div class="icon-wraper">
+                        <i class="ion-ribbon-a"></i>
+                    </div>
+                    <div class="content">
+                        <h4 class="bottom-line"><span class="markFont">ของรางวัลสำหรับผู้เข้าร่วมงาน</span></h4>
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp;เพียงเข้ามาร่วมชมการแข่งขันสดๆ รอบ 4 ทีมสุดท้ายในวันงาน ก็มีสิทธิ์ได้รางวัลจาก Garena Thailand เช่นกัน โดยเราจะสุ่มผู้โชคดีได้รับ skin ในเกม จำนวน 10 รางวัล และเสื้อยืด RoV สุดเท่ห์อีก 5 รางวัล</p>
                     </div>
                 </div>
                 <div class="feature-wiget">
@@ -27,13 +56,11 @@
                     </div>
                     <div class="content">
                         <h4 class="bottom-line"><span class="markFont">ทีมนี้พี่รัก ทีมนั้นน้องเชียร์</span></h4>
-                        <p>&nbsp;&nbsp;&nbsp;&nbsp;ก๊วนไหน แก็คไหน ไม่ผ่านรอบคัดเลือก  ก็มีสิทธิ์ได้รางวัลเป็น Coin ในเกม จาก Garena Thailand ผู้เข้าร่วมการแข่งขัน ทุกท่าน หากเข้ามาร่วมชมสดๆ ในวันงาน ก็อาจเป็นผู้โชคดีได้เป็นเศรษฐีหรือไอเทมดีๆในเกม</p>
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp;ก๊วนไหน แก็คไหน ไม่ผ่านรอบคัดเลือก ก็มีสิทธิ์ได้รางวัล Popular Vote จากคลิป video แนะนำตัวที่แนบมาตอนสมัครเข้าร่วมแข่งขันด้วยเช่นกัน </p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
-
-
                 <div class="ui one column grid">
                     <div class="column">
                         <div class="ui raised segment">
@@ -42,9 +69,6 @@
                         </div>
                     </div>
                 </div>
-
-
-
                 <br/>
                 <p>
                 <div class="photo sweat16_blur" data-stellar-background-ratio="0.5"></div>
