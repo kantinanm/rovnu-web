875a18d4a44e1ed93c101d9ec57795ab9225abe5
basicgts <basicgts@gmail.com>
2018-05-28 21:51:52 +0700

    edit rule page add icon and env config

--- app/Http/Controllers/HomeRovController.php
+++ app/Http/Controllers/HomeRovController.php
@@ -58,9 +58,16 @@ class HomeRovController extends Controller
         $allowTeamRegister =Config::get('app.allow_team_register');
         $allowPaticipantRegister =Config::get('app.allow_paticipant_register');
         $allowSponsorRegister =Config::get('app.allow_sponsor_register');
+        $register_url =Config::get('app.how_to_register_url');
+        $clip_video_url =Config::get('app.how_to_make_video_url');
+        $register_manual=Config::get('app.how_to_register_manual');
+
 
         return view('pages.rules')->with(compact('allowTeamRegister',$allowTeamRegister))
             ->with(compact('allowPaticipantRegister',$allowPaticipantRegister))
-            ->with(compact('allowSponsorRegister',$allowSponsorRegister));
+            ->with(compact('allowSponsorRegister',$allowSponsorRegister))
+            ->with(compact('register_url',$register_url))
+            ->with(compact('clip_video_url',$clip_video_url))
+            ->with(compact('register_manual',$register_manual));
     }
 }
