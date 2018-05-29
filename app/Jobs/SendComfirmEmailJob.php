9871e8235f0b67762d52bb2f594e1e91f5c34abf
basicgts <basicgts@gmail.com>
2018-05-30 03:00:57 +0700

    add job confirmation data , mail view

---
+++ app/Jobs/SendComfirmEmailJob.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Mail\SendConfirmationMailable;
+use Mail;
+use Illuminate\Mail\Mailer;
+use App\User;
+use Config;
+
+class SendComfirmEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public $user;
+    public function __construct(User $user)
+    {
+        //
+        $this->user=$user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+        Mail::to($this->user->email)
+            ->send(new SendConfirmationMailable($this->user));
+    }
+}
---
+++ app/Mail/SendConfirmationMailable.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\User;
+use App\Player;
+use Config;
+
+class SendConfirmationMailable extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $user;
+
+    public function __construct(User $user)
+    {
+        //
+        $this->user=$user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        //return $this->view('view.name');
+        $admin_email =Config::get('app.admin_address');
+        $manager_email =Config::get('app.manager_address');
+
+        $players=Player::where('team_id',$this->user->id)->get();
+
+        return $this->subject("ยืนยันการส่งทีมเข้าร่วมแข่งขัน NU e-Sport ROV Tournament ")
+            ->from('ecpe-software@nu.ac.th')
+            ->bcc($admin_email)
+            ->view('email.confirmTeamView')->with(compact('user',$this->user)) ->with(compact('players',$players));
+
+    }
+}
