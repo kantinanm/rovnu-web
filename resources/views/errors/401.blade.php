26a2b6b38c4541968eed3a5a4bdf499d8097775b
basicgts <basicgts@gmail.com>
2018-05-30 01:43:15 +0700

    edit default error page

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,15 +1,86 @@
-@extends('layouts.app')
+@extends('layouts.default')
+@section('title')
+    Error 401
+@stop
+@section('css_script')
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/owl.carousel.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/owl.theme.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+
+    <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="{{ URL::asset('css/stellar.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('js/Semantic-UI-CSS/semantic.css') }}">
+@endsection
+@include('includes.header_v2')
+
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Error 401</div>
+    <section id="introduction" class="gradient-violat padding-top-90 home-slider">
+        <div id="home-slider" class="empty-carousel">
 
-                    <h2>{{ $exception->getMessage() }}</h2>
+        </div>
+    </section>
+
+    <section id="formUI" class="newsletter">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+
+                    <div class="ui error message">
+                        <i class="close icon"></i>
+                        <div class="header">
+                            Error 401
+                        </div>
+                        <ul class="list">
+                            <li>{{ $exception->getMessage() }}</li>
+
+                        </ul>
+                    </div>
+
+
                 </div>
             </div>
         </div>
+
+    </section>
+@endsection
+@section('js_plugin')
+    <div id="scroll-top-div" class="scroll-top-div">
+        <div class="scroll-top-icon-container">
+            <i class="ion-ios-arrow-thin-up"></i>
+        </div>
     </div>
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <!--<script src="js/jquery.min.js"></script> -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+    <script src="{{ URL::asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{ URL::asset('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ URL::asset('js/script.js') }}"></script>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+    <script src="{{ URL::asset('js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ URL::asset('js/stellar/jquery.stellar.min.js') }}"></script>
+    <script src="{{ URL::asset('js/Semantic-UI-CSS/semantic.min.js') }}"></script>
+
+    <script>
+        jQuery(document).ready(function( $ ) {
+
+        });
+
+
+    </script>
+
+    <script>
+
+    </script>
 @endsection
