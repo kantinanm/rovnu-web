eae7119f56b3ed3538168e4dfa2eb18fa323ecb1
matzeeya <matzeeya@gmail.com>
2018-06-15 11:29:31 +0700

    edit paticipant

--- app/Paticipant.php
+++ app/Paticipant.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Paticipant extends Model
 {
     //
+    public $table = "paticipant";
     protected $fillable = [
-        'fullname', 'email', 'garena_id', 'gender', 'age','provice','member_type','choice1' ,'choice2','choice3','choice4','choice5','choiceetc','nuchoice1' ,'nuchoice2','nuchoice3','nuchoice4','nuchoice5','nuchoiceetc'
+        'fullname', 'email', 'garena_id', 'gender', 'age','provice','member_type','choice1' ,'choice2','choice3','choice4','choice5','choice6','choiceetc','nuchoice1' ,'nuchoice2','nuchoice3','nuchoice4','nuchoice5','nuchoice6','nuchoiceetc'
     ];
 
     protected $primaryKey = 'p_id';
