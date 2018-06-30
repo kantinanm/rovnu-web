fcd34a2d2fa160d2d430c14e2cb5871671ff0a00
basicgts <basicgts@gmail.com>
2018-07-01 00:40:35 +0700

    update wating for team list approve

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -54,37 +54,43 @@
             </div> <!--end row2-->
 
             <div class="row">
-                <div class="col-md-3 text-center">
-                    <span class="counter" style="display: inline-block; width: 25%;font-size: 40px;">{{$playerConfirmTeam}}</span>
+                <div class="col-md-4 text-center">
+                    <span class="counter" style="display: inline-block; width: 25%;font-size: 40px;">{{$playerConfirmTeam+7}}</span>
                 </div> <!--col-md-12-->
-                <div class="col-md-3 text-center">
+                <div class="col-md-4 text-center">
                     <span class="counter" style="display: inline-block; width: 25%;font-size: 40px;">{{$teamConfirm}}</span>
                 </div> <!--col-md-12-->
-                <div class="col-md-3 text-center">
-                    <span class="counter" style="display: inline-block; width: 25%;font-size: 40px;">{{$teamActive}}</span>
-                </div> <!--col-md-12-->
-                <div class="col-md-3 text-center">
-                    <span class="counter" style="display: inline-block; width: 25%;font-size: 40px;">{{$playerPaticipant}}</span>
-                </div> <!--col-md-12-->
+                 <div class="col-md-4 text-center">
+                    <span class="counter" style="display: inline-block; width: 25%;font-size: 40px;">4</span>
+                </div>
+               <!--  <div class="col-md-3 text-center">
+                    <span class="counter" style="display: inline-block; width: 25%;font-size: 40px;"></span>
+                </div> -->
             </div> <!--end row2-->
 
             <div class="row">
-                <div class="col-md-3 text-center">
+                <div class="col-md-4 text-center">
                     <i class="ion-android-contacts" style="font-size: 30px;"></i> ผู้เข้าแข่งขัน
                 </div> <!--col-md-4-->
-                <div class="col-md-3 text-center">
-                    <i class="ion-ios-game-controller-b-outline" style="font-size: 30px;"></i> ทีมที่ยืนยันแล้ว
+                <div class="col-md-4 text-center">
+                    <i class="ion-ios-game-controller-b-outline" style="font-size: 30px;"></i> ทีมที่เข้าร่วม
                 </div> <!--col-md-4-->
-                <div class="col-md-3 text-center">
-                    <i class="hourglass start icon" style="font-size: 30px;"></i>  ทีมที่รอยืนยัน
+                <div class="col-md-4 text-center">
+                    <i class="hourglass start icon" style="font-size: 30px;"></i>  ทีมที่รอพิจารณาใหม่
                 </div> <!--col-md-4-->
-                <div class="col-md-3 text-center">
-                    <i class="ion-person-stalker" style="font-size: 30px;"></i> ผู้เล่นที่รอยืนยันทีม
-                </div> <!--col-md-4-->
+
 
             </div> <!--end row3-->
 
+
         </div><!--container-->
+    </section>
+    <section id="testimonial2" class="testimonial-section padding-top-bottom-90" style="background-color: #880000">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p class="text-white">* เราจะประกาศรายชื่อทีมอย่างเป็นทางการ วันนี้ หลัง 16.00 น.</p>
+            </div>
+        </div> <!--end row3-->
     </section>
     @include('includes.home.modal')
 @endsection
