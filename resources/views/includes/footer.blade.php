aef189c52686af4d7f8eda38fd0b14286ac61064
basicgts <basicgts@gmail.com>
2020-02-18 14:23:02 +0700

    update register manual

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -44,8 +44,8 @@
                             <h4>เกี่ยวกับเรา</h4>
                             <div class="list-group">
                                 <a href="{{ route('place') }}" class="list-group-item"><i class="ion-videocamera" style="font-size: 30px;"></i>&nbsp;<span class="markFont">สถานที่จัดงาน</span></a>
-                            <!--<a href="https://www.youtube.com/watch?v=BXBurSbShYk" target="_blank" class="list-group-item"><i class="youtube icon" ></i>&nbsp;<span class="markFont">ตัวอย่างการสมัคร</span></a>
-                                <a href="https://goo.gl/iJkFXw" target="_blank" class="list-group-item"><i class="ion-ios-list-outline" ></i>&nbsp;<span class="markFont">คู่มือการสมัคร</span></a>-->
+                            <!--<a href="https://www.youtube.com/watch?v=BXBurSbShYk" target="_blank" class="list-group-item"><i class="youtube icon" ></i>&nbsp;<span class="markFont">ตัวอย่างการสมัคร</span></a>-->
+                                <a href="http://bit.ly/2SmCsYF" target="_blank" class="list-group-item"><i class="ion-ios-list-outline" ></i>&nbsp;<span class="markFont">คู่มือการสมัคร</span></a>
 
                                 <a href="https://www.facebook.com/%E0%B8%AD%E0%B8%B5%E0%B8%AA%E0%B8%9B%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%95-%E0%B8%81%E0%B8%B5%E0%B8%AC%E0%B8%B2%E0%B8%9A%E0%B8%B8%E0%B8%84%E0%B8%A5%E0%B8%B2%E0%B8%81%E0%B8%A3-%E0%B8%A1%E0%B8%99%E0%B9%80%E0%B8%A3%E0%B8%A8%E0%B8%A7%E0%B8%A3-104781621097309/" target="_blank" class="list-group-item"><i class="ion-social-facebook" style="font-size: 30px;"></i>&nbsp;<span class="markFont">Facebook Page</span></a>
                             </div>
